00142ce0f9d38e944382d504b4b6795e476d287a
Seth Wilson <sethhwilson@gmail.com>
2021-10-09 22:26:03 -0500

    Directly link to Poets on front page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Link from "../components/Link";
 
 export default function Index() {
   return (
@@ -16,6 +17,7 @@ export default function Index() {
         </Typography>
         <p>Welcome to a collection of some of the greatest poetry of all time, powered by the excellent PoetryDB!</p>
         <p>This is very much a work in progress - stay tuned for enhancements.</p>
+        <Link href="/poets" color="inherit">Check out the poets!</Link>
       </Box>
     </Container>
   );
